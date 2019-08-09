18717a1bb01d80f0a0754db89fe7fdcff421fd83
Akshay Damodaran <Akshay.d@browserstack.com>
2019-08-09 16:08:17 +0530

    Renamed delete to destroy since it is a reserved keyword

---
+++ models/modality/destroy.js
@@ -0,0 +1,27 @@
+const codes = {
+  err:
+  {
+    code: 402,
+    mesaage: "Error in deleting modality"
+  },
+  success: {
+    code: 200,
+    data: "Successfully deleted modality"
+  }
+}
+
+exports.destroy = (connection,payload) => {
+  return new Promise((resolve, reject) => {
+    let {id} =payload;
+    let query=`delete from modality where id=${id}`
+    connection.query(query, (err, res, feild) => {
+      if (err)
+        reject(codes.err)
+      else {
+        console.log(err, res)
+        codes.success.data = res;
+        resolve(codes.success);
+      }
+    });
+  });
+}
