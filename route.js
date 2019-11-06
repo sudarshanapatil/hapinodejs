abbd7b90b59ed840860b81d62803b7a2be869990
Sudarshana Patil <sudarshanagopinath.patil@philips.com>
2019-11-06 23:42:30 +0530

    chatbot chnages

--- route.js
+++ route.js
@@ -1,14 +1,14 @@
 const modRoutes = require('./routes/modality/route')
 const prodRoutes = require('./routes/product/route')
-const userRoute =require('./routes/user/route')
+//const userRoute =require('./routes/user/route')
 
 //All Routes are defined here
 exports.configureRoutes = (server, connection,save) => {
     //get all routes for Modality 
     let modArr = modRoutes.modRoutes(connection,save)
     let prodArr = prodRoutes.prodRoutes(connection,save)
-    let userArr =userRoute.userRoutes(connection,save)
-    let allRoutes = modArr.concat(prodArr,userArr)
+    //let userArr =userRoute.userRoutes(connection,save)
+    let allRoutes = modArr.concat(prodArr)
 
     //get all routes for products
     for (let i in allRoutes)
